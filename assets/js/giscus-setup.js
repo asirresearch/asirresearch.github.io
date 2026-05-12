1935ab8e29d17a4a384c520486462ecffd7989cf
asirresearch <asirresearch@outlook.com>
2026-05-12 14:33:36 +0000

    Deploying to gh-pages from @ asirresearch/asirresearch.github.io@c7e83b3a676d77c5c22471eb3b3cfaee7a3d214e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "asirresearch/asirresearch",
+    "data-repo": "asirresearch/asirresearch.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
